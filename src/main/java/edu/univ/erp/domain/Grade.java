5e7a29c7f691d4b9a2219e9c5c768cfd637cec39
Darshan Bothra <hitmandarshan10@gmail.com>
2025-10-05 16:25:50 +0530

    Created Grade.java

---
+++ src/main/java/edu/univ/erp/domain/Grade.java
@@ -0,0 +1,47 @@
+package edu.univ.erp.domain;
+
+public class Grade {
+    // fetch from db
+
+    private int grade_id; // auto-increment in table erp_db.grades
+    private int enrollment_id; // query erp_db.enrollments with student_id and section_id (fetch from instructor entering grade from selected section)
+    private String component; // quiz, midsem or endsem
+    private double score; // marks scored in component
+    private double final_grade; // adjust score according to weightage
+
+    Grade(int grade_id, int enrollment_id, String component, double score, double final_grade){
+        this.enrollment_id = enrollment_id;
+        this.grade_id = grade_id;
+        this.component = component;
+        this.score = score;
+        this.final_grade = final_grade;
+    }
+
+    // getters
+    public int getEnrollmentId(){
+        return this.enrollment_id;
+    }
+
+    public int getGradeId(){
+        return this.grade_id;
+    }
+
+    public String getComponent(){
+        return this.component;
+    }
+
+    public double getScore(){
+        return this.score;
+    }
+
+    public double getFinalGrade(){
+        return this.final_grade;
+    }
+
+    // String Representation
+
+    @Override
+    public String toString(){
+        return String.format("Grade {GradeId: %d, EnrollmentId: %d, Component: %s, Score: %f, FinalGrade: %f}\n", this.getGradeId(), this.getEnrollmentId(), this.getComponent(), this.getScore(), this.getFinalGrade());
+    }
+}
